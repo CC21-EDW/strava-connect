92fcb9752101e5ea1e03fe67e991ba7e88a7cbd4
Arthur Neudeck <arthur.neudeck@baloise.ch>
2022-01-21 07:32:35 +0100

    #10: reformatted code

--- src/test/java/com/baloise/open/strava/edw/infrastructure/kafka/AbstractKafkaTest.java
+++ src/test/java/com/baloise/open/strava/edw/infrastructure/kafka/AbstractKafkaTest.java
@@ -17,18 +17,18 @@ import java.util.Properties;
 @Testcontainers
 public class AbstractKafkaTest {
 
-  @Container
-  public static KafkaContainer kafkaTestContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.3.2"));
+    @Container
+    public static KafkaContainer kafkaTestContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.3.2"));
 
-  protected Properties getTestContainerProperties() {
-    final Properties properties = new Properties();
-    properties.put(Config.KAFKA_SERVER_CONFIG_KEY, kafkaTestContainer.getBootstrapServers());
-    properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaTestContainer.getBootstrapServers());
-    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
-    properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-    properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
-    properties.put(Config.SCHEMA_SERVER_CONFIG_KEY, "mock://localhost");
-    return properties;
-  }
+    protected Properties getTestContainerProperties() {
+        final Properties properties = new Properties();
+        properties.put(Config.KAFKA_SERVER_CONFIG_KEY, kafkaTestContainer.getBootstrapServers());
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaTestContainer.getBootstrapServers());
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        properties.put(Config.SCHEMA_SERVER_CONFIG_KEY, "mock://localhost");
+        return properties;
+    }
 }
--- src/test/java/com/baloise/open/strava/edw/infrastructure/kafka/ScanTopicTest.java
+++ src/test/java/com/baloise/open/strava/edw/infrastructure/kafka/ScanTopicTest.java
@@ -38,116 +38,116 @@ import static io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig.SCHEMA
 @Disabled
 public class ScanTopicTest extends AbstractKafkaTest {
 
-  private static final String TOPIC_NAME = "lz.edw.strava-connect.activity";
+    private static final String TOPIC_NAME = "lz.edw.strava-connect.activity";
 
-  @Test
-  @Disabled("Currently turned of as we will invest time in kafka streams later")
-  public void readPartition() {
-    KafkaConsumer<String, ActivityDto> consumer = new KafkaConsumer<>(getConsumerProperties());
+    @Test
+    @Disabled("Currently turned of as we will invest time in kafka streams later")
+    public void readPartition() {
+        KafkaConsumer<String, ActivityDto> consumer = new KafkaConsumer<>(getConsumerProperties());
 
-    Set<TopicPartition> topicPartitions = getTopicPartition(consumer, TOPIC_NAME);
-    consumer.assign(topicPartitions);
+        Set<TopicPartition> topicPartitions = getTopicPartition(consumer, TOPIC_NAME);
+        consumer.assign(topicPartitions);
 
-    ArrayList<ActivityDto> activities = new ArrayList<>();
-    consumer.seekToBeginning(topicPartitions);
-    ConsumerRecords<String, ActivityDto> records = consumer.poll(Duration.ofDays(1));
-    records.forEach((record) -> {
-      try {
-        ActivityDto activity = record.value();
-        log.info("Record: {}", activity);
-        activities.add(activity);
-      } catch (Throwable e) {
-        log.error("Issue converting AVRO", e);
-        Assertions.fail("An exception occurred - please look into console.");
-      }
-    });
+        ArrayList<ActivityDto> activities = new ArrayList<>();
+        consumer.seekToBeginning(topicPartitions);
+        ConsumerRecords<String, ActivityDto> records = consumer.poll(Duration.ofDays(1));
+        records.forEach((record) -> {
+            try {
+                ActivityDto activity = record.value();
+                log.info("Record: {}", activity);
+                activities.add(activity);
+            } catch (Throwable e) {
+                log.error("Issue converting AVRO", e);
+                Assertions.fail("An exception occurred - please look into console.");
+            }
+        });
 
-    //provide a clear
-    for (ActivityDto currentActivity : activities) {
-      log.info("Entity: {}", currentActivity);
+        //provide a clear
+        for (ActivityDto currentActivity : activities) {
+            log.info("Entity: {}", currentActivity);
+        }
     }
-  }
 
-  /**
-   * Provides the collection of topic partitions we need to brows a topic.
-   *
-   * @param consumer  the consumer we use to access the topic.
-   * @param topicName the topic we want to access.
-   * @return the collection, must not be null.
-   */
-  private Set<TopicPartition> getTopicPartition(KafkaConsumer<?, ?> consumer, String topicName) {
-    List<PartitionInfo> partitionInfos = consumer.partitionsFor(topicName);
-    return partitionInfos
-        .stream()
-        .map(partitionInfo -> new TopicPartition(partitionInfo.topic(), partitionInfo.partition()))
-        .collect(Collectors.toSet());
-  }
+    /**
+     * Provides the collection of topic partitions we need to brows a topic.
+     *
+     * @param consumer  the consumer we use to access the topic.
+     * @param topicName the topic we want to access.
+     * @return the collection, must not be null.
+     */
+    private Set<TopicPartition> getTopicPartition(KafkaConsumer<?, ?> consumer, String topicName) {
+        List<PartitionInfo> partitionInfos = consumer.partitionsFor(topicName);
+        return partitionInfos
+                .stream()
+                .map(partitionInfo -> new TopicPartition(partitionInfo.topic(), partitionInfo.partition()))
+                .collect(Collectors.toSet());
+    }
 
-  @Test
-  @Disabled("Currently turned of as we will invest time in kafka streams later")
-  public void checkActivityTopic() {
-    StreamsBuilder builder = new StreamsBuilder();
+    @Test
+    @Disabled("Currently turned of as we will invest time in kafka streams later")
+    public void checkActivityTopic() {
+        StreamsBuilder builder = new StreamsBuilder();
 
-    StoreBuilder<KeyValueStore<String, ActivityDto>> store =
-        Stores.keyValueStoreBuilder(
-            Stores.inMemoryKeyValueStore("inmemory"),
-            Serdes.String(),
-            getActivityAvroSerde((String) getProducerProperties().get(Config.SCHEMA_SERVER_CONFIG_KEY)));
-    builder.addStateStore(store);
+        StoreBuilder<KeyValueStore<String, ActivityDto>> store =
+                Stores.keyValueStoreBuilder(
+                        Stores.inMemoryKeyValueStore("inmemory"),
+                        Serdes.String(),
+                        getActivityAvroSerde((String) getProducerProperties().get(Config.SCHEMA_SERVER_CONFIG_KEY)));
+        builder.addStateStore(store);
 
-    KStream<String, ActivityDto> sourceStream = builder.stream(TOPIC_NAME, Consumed.with(Serdes.String(),
-        getActivityAvroSerde((String) getProducerProperties().get(Config.SCHEMA_SERVER_CONFIG_KEY))));
-    sourceStream.print(Printed.toSysOut());
+        KStream<String, ActivityDto> sourceStream = builder.stream(TOPIC_NAME, Consumed.with(Serdes.String(),
+                getActivityAvroSerde((String) getProducerProperties().get(Config.SCHEMA_SERVER_CONFIG_KEY))));
+        sourceStream.print(Printed.toSysOut());
 
-    KTable<String, ActivityDto> textLinesTable =
-        builder.table(TOPIC_NAME, Consumed.with(Serdes.String(),
-            getActivityAvroSerde((String) getProducerProperties().get(Config.SCHEMA_SERVER_CONFIG_KEY))));
+        KTable<String, ActivityDto> textLinesTable =
+                builder.table(TOPIC_NAME, Consumed.with(Serdes.String(),
+                        getActivityAvroSerde((String) getProducerProperties().get(Config.SCHEMA_SERVER_CONFIG_KEY))));
 
-    final Topology topology = builder.build();
-    final KafkaStreams streams = new KafkaStreams(topology, getProducerProperties());
-    streams.start();
+        final Topology topology = builder.build();
+        final KafkaStreams streams = new KafkaStreams(topology, getProducerProperties());
+        streams.start();
 
-    Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-  }
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
 
-  /**
-   * Provides the specific AVRO serializer/ deserializer (SerDe) we need using KafkaStreams.
-   *
-   * @param schemaUrl URL we need to access the schema.
-   * @return the specific AVRO serializer/ deserializer.
-   */
-  private SpecificAvroSerde<ActivityDto> getActivityAvroSerde(String schemaUrl) {
-    SpecificAvroSerde<ActivityDto> movieAvroSerde = new SpecificAvroSerde<>();
+    /**
+     * Provides the specific AVRO serializer/ deserializer (SerDe) we need using KafkaStreams.
+     *
+     * @param schemaUrl URL we need to access the schema.
+     * @return the specific AVRO serializer/ deserializer.
+     */
+    private SpecificAvroSerde<ActivityDto> getActivityAvroSerde(String schemaUrl) {
+        SpecificAvroSerde<ActivityDto> movieAvroSerde = new SpecificAvroSerde<>();
 
-    Map<String, String> serdeConfig;
-    serdeConfig = new HashMap<>();
-    serdeConfig.put(SCHEMA_REGISTRY_URL_CONFIG, schemaUrl);
-    movieAvroSerde.configure(
-        serdeConfig, false);
-    return movieAvroSerde;
-  }
+        Map<String, String> serdeConfig;
+        serdeConfig = new HashMap<>();
+        serdeConfig.put(SCHEMA_REGISTRY_URL_CONFIG, schemaUrl);
+        movieAvroSerde.configure(
+                serdeConfig, false);
+        return movieAvroSerde;
+    }
 
 
-  /**
-   * Provides the configuration we use specifically for producing events.
-   *
-   * @return the properties ready to be used.
-   */
-  private Properties getProducerProperties() {
+    /**
+     * Provides the configuration we use specifically for producing events.
+     *
+     * @return the properties ready to be used.
+     */
+    private Properties getProducerProperties() {
 
-    Properties properties = getTestContainerProperties();
-     properties.put("application.id", "test-query");
-    return properties;
-  }
+        Properties properties = getTestContainerProperties();
+        properties.put("application.id", "test-query");
+        return properties;
+    }
 
-  /**
-   * Provides the configuration we use specifically for consuming events.
-   *
-   * @return the properties ready to be used.
-   */
-  private Properties getConsumerProperties() {
-    Properties properties = getTestContainerProperties();
-    properties.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
-    return properties;
-  }
+    /**
+     * Provides the configuration we use specifically for consuming events.
+     *
+     * @return the properties ready to be used.
+     */
+    private Properties getConsumerProperties() {
+        Properties properties = getTestContainerProperties();
+        properties.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        return properties;
+    }
 }
